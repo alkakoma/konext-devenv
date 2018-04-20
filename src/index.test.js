ec5cbf75f164dcfcc81baa7e0e4a633f9b02be83
Administrator <administrator@konext.local>
2018-04-20 16:42:32 +0200

    home change

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,21 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('My first test', () => {
     it('should pass', () => {
-        expect(true).to.equal (true)
+        expect(true).to.equal(true)
     });
 });
 
 describe('index.html', () => {
     it('should have users in h1', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
-        jsdom.env(index, function (err, window) {
+        jsdom.env(index, function(err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHtml).to.equal("Users") ;
+            expect(h1.innerHtml).to.equal('Users');
             done();
             window.close();
         });
     });
 });
-
